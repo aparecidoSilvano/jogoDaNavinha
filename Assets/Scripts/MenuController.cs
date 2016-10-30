1322ed6cf21d2b36cfcc07044d14908e53546947
silvano aparecido albuquerque <silvano aparecido albuquerque>
2016-10-29 23:55:13 -0300

    Adicionando menu inicial do jogo da navinha.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*
+	 * link do tutorial de como fazer menu:
+	 * https://www.youtube.com/watch?v=PEmNMI1JUvI
+	 * 
+	 */
+
+	public void bt_start() {
+		Application.LoadLevel("MainScenes");
+	}
+
+	public void bt_option() {
+		Debug.Log("Para abrir a tela de opções");
+	}
+
+	public void bt_exit() {
+		Debug.Log("para sair do jogo");
+		Application.Quit ();
+	}
+}
